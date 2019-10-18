b52debf37cc49c3412025d0ed982eee79eac9c6a
Scott Matsuda <smatsuda2014@gmail.com>
2019-10-18 18:24:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,7 @@ return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	return programmer_hash[:alan_kay][:known_for]
+	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +36,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	return programmer_hash[:alan_kay][:known_for]
+     
 end
 
 def dennis_ritchies_language
