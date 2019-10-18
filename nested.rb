44ab89114f5e5a339821f81f24865e538b348017
Scott Matsuda <smatsuda2014@gmail.com>
2019-10-18 18:17:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,6 +61,7 @@ def adding_matz
 programmer_hash[0] = {:yukihiro_matsumoto => {
   :known_for => "Ruby",
     :languages => ["LISP", "C"]
+                    }
 }
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
